a2018987e7b04933cb0b2dcc430859666ad3bc87
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-11-23 16:57:34 +0000

    Added print lines to show "check" for conditions.

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line 👇
 if year % 4 == 0:
-  #print("Year is evenly divisible by 4")
+  print("Year is evenly divisible by 4 - check")
   if year % 100 == 0:
-    #print("Year is divisible by 100")
+    print("Year is also divisible by 100 - check")
     if year % 400 == 0:
+      print("Year is also divisible by 400 - check")
       print(f"The year {year} is a leap year")
     else:
+        print("BUT year is not divisible by 400, so...")
         print(f"The year {year} is NOT a leap year")
   else:
     print(f"The year {year} is a leap year")
